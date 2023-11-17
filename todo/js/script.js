efa45d43029cdbcd73e1b4ff52327f189ac5af1f
Tom GERNEZ <tgernez@student.42.fr>
2023-11-17 23:04:58 +0100

    add basics

--- todo/js/script.js
+++ todo/js/script.js
@@ -1,23 +1,40 @@
 // Add Button
 var addButton = document.getElementById("add-button");
+addButton.addEventListener("click", addTask);
 
-addButton.addEventListener("click", function() {
+function addTask() {
     const taskname = prompt("Enter the task name");
     const lowercase_taskname = taskname.toLowerCase();
+    const div = document.createElement("div");
+    div.id = lowercase_taskname;
+    const button = document.createElement("button");
+    button.id = "delete-" + lowercase_taskname;
+    button.className = "delete-button";
+    button.title = "Delete?";
+    button.innerHTML = "X";
+    button.addEventListener('click', function() {
+        this.parentNode.remove();
+    });
+
     const box = document.createElement("input");
     box.type = "checkbox";
-    box.id=lowercase_taskname;
-    box.name=lowercase_taskname;
-    box.value=lowercase_taskname;
+    box.id = lowercase_taskname;
+    box.name = lowercase_taskname;
+    box.value = lowercase_taskname;
+    
     const label = document.createElement("label");
-    label.htmlFor=lowercase_taskname;
+    label.htmlFor = lowercase_taskname;
     label.appendChild(document.createTextNode(" " + taskname));
-    const br = document.createElement("br");
+    
     const element = document.getElementById("todo-fieldset");
-    element.appendChild(box);
-    element.appendChild(label);
-    element.appendChild(br);
-});
+    div.appendChild(button);
+    div.appendChild(box);
+    div.appendChild(label);
+    element.appendChild(div);
+};
 
-var span = document.getElementById("span-todo-1");
-span.remove();
+document.addEventListener('keydown', function(event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+});
