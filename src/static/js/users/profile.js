633e0b55ca45700075f455cd0971551a71e3739a
Tag6362 <tag6362@gmail.com>
2024-02-27 20:34:06 +0100

    add profile popup for user

--- src/static/js/users/profile.js
+++ src/static/js/users/profile.js
@@ -1,5 +1,6 @@
 const profilePopup = document.getElementById("profile-popup");
 
+// Open for History
 document.querySelectorAll(".history-profile-picture").forEach(element => {
 	element.addEventListener('click', (event) => {
 		event.stopPropagation();
@@ -9,10 +10,29 @@ document.querySelectorAll(".history-profile-picture").forEach(element => {
 	});
 });
 
-
-// TODO check if clicked element isn't a button within the popup
+// Close for History
 document.addEventListener('click', (event) => {
 	if (!profilePopup.contains(event.target) && !event.target.matches('.history-profile-picture')) {
+		event.stopPropagation();
+		profilePopup.classList.add("d-none");
+		profilePopup.classList.remove("d-block");
+		unblur_background();
+	}
+});
+
+// Open for Friendships
+document.querySelectorAll(".friendships-profile").forEach(element => {
+	element.addEventListener('click', (event) => {
+		event.stopPropagation();
+		profilePopup.classList.remove("d-none");
+		profilePopup.classList.add("d-block");
+		blur_background();
+	});
+});
+
+// Close for Friendships
+document.addEventListener('click', (event) => {
+	if (!profilePopup.contains(event.target) && !event.target.matches('.friendships-profile-picture')) {
 		event.stopPropagation();
 		profilePopup.classList.add("d-none");
 		profilePopup.classList.remove("d-block");
